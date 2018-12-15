49a008349f9888573651810a484323bcd36b7877
12Me21 <12Me21.mc@gmail.com>
2018-12-15 11:02:55 -0500

    copy

--- parse2.py
+++ parse2.py
@@ -565,7 +565,7 @@ if len(args)>=3:
 	# copy css file
 	css = os.path.join(args[1],"style.css")
 	if os.path.isfile(css):
-		shutil.copy2(css, args[2])
+		shutil.copy(css, args[2])
 	else:
 		print("Warning: Missing CSS file in input")
 	# convert all pages
